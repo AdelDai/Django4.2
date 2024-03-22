d6fda265d9eab565e783cec86c9f2eb19051a89a
Adel Giniatov <adel-giniatoff@yandex,ru>
2024-03-22 14:55:07 +0300

    djando

--- lyceum/catalog/models.py
+++ lyceum/catalog/models.py
@@ -12,20 +12,11 @@ def perfect_validator(value):
     raise ValidationError("нету слово роскошно или превосходно")
 
 
-class Item(django.db.models.Model):
-    is_published = django.db.models.BooleanField(
-        "опубликовано",
-        default=True,
-    )
-    name = django.db.models.CharField("название", max_length=150)
-    tags = models.ManyToManyField(tags =)
-
-    text = django.db.models.TextField("текст", validators=[perfect_validator])
-
-
 class Tag(django.db.models.Model):
-    name = django.db.models.CharField("название", max_length=150,)
-    categors = django.db.models.ManyToManyField(categors =)
+    name = django.db.models.CharField(
+        "название",
+        max_length=150,
+    )
 
     is_published = django.db.models.BooleanField(
         "опубликовано",
@@ -33,20 +24,24 @@ class Tag(django.db.models.Model):
     )
 
     slug = django.db.models.SlugField(
-        "слаг",
-        max_length=200,
-        unique=True,
-    )
+        "слаг", max_length=200, unique=True,)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Теги"
 
 
 class Category(django.db.models.Model):
-    is_published = django.db.models.BooleanField(
+    is_published = models.BooleanField(
         "опубликовано",
         default=True,
     )
-    name = django.db.models.CharField("название", max_length=150, )
-    items = django.db.models.ManyToManyField(items = )
-
+    name = models.CharField(
+        "название",
+        max_length=150,
+    )
 
     slug = django.db.models.SlugField(
         "Слаг",
@@ -54,8 +49,26 @@ class Category(django.db.models.Model):
         unique=True,
     )
     weight = django.db.models.IntegerField(
-        "Вес",
-        default=100,
-        null=True,
-        blank=True,
+        "Вес", default=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Категории"
+class Item(django.db.models.Model):
+    is_published = django.db.models.BooleanField(
+        "опубликовано",
+        default=True,
     )
+    tags = models.ManyToManyField(Tag, related_name="items", verbose_name="теги")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="категория")
+    name = django.db.models.CharField("название", max_length=150)
+    text = django.db.models.TextField("текст", validators=[perfect_validator])
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib import admin
 from django.urls import include, path
 
 
@@ -6,6 +7,7 @@ urlpatterns = [
     path("", include("homepage.urls")),
     path("about/", include("about.urls")),
     path("catalog/", include("catalog.urls")),
+    path("admin/", admin.site.urls)
 ]
 
 if settings.DEBUG:
